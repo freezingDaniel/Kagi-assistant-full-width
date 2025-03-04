a417f9b1332a039f02a75f68ba00937cc2378d2e
freezingDaniel <82905307+freezingDaniel@users.noreply.github.com>
2025-03-04 13:25:53 +0000

    Update meta-data and remove depricated code

--- kagi-assistant-full-width.user.js
+++ kagi-assistant-full-width.user.js
@@ -1,9 +1,10 @@
 // ==UserScript==
 // @name         Resize Kagi Assistant to use full width
-// @namespace    http://tampermonkey.net/
-// @version      2025-02-28
 // @description  Resize Kagi Assistant to use full width
 // @author       freezingDaniel
+// @namespace    http://tampermonkey.net/
+// @version      2025-03-04
+// @supportURL   https://github.com/freezingDaniel/Kagi-assistant-full-width/issues
 // @match        https://kagi.com/assistant*
 // @icon         https://kagi.com/favicon-assistant-32x32.png
 // @grant        GM_addStyle
@@ -11,18 +12,3 @@
 
 //GM_addStyle('.main-center-box > :first-child { width: calc(100% - 169px) !important }');
 GM_addStyle('.main-center-box > :first-child { max-width: 100%; !important }');
-
-//GM_addStyle('.main-center-box>* { margin-inline: auto; max-width: calc(100% - 169px); padding-inline: 16px;width: 100%; }');
-
-
-//(function() {
-//    'use strict';
-//
-//    // Deprecated - Old Assistant
-//    //document.documentElement.style.setProperty('--center_content_width', "100%");
-//
-//    const elements = document.querySelectorAll('.main-center-box > :first-child');
-//        elements.forEach(element => {
-//            element.style.width = 'calc(100% - 169px)';
-//        });
-//})();
