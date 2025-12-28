2d9527147ed2f19ac724da81fd05f4debe04bf4f
freezingDaniel <82905307+freezingDaniel@users.noreply.github.com>
2025-12-28 13:12:59 +0100

    Update version and adjust style for full width

--- kagi-assistant-full-width.user.js
+++ kagi-assistant-full-width.user.js
@@ -3,12 +3,11 @@
 // @description  Resize Kagi Assistant to use full width
 // @author       freezingDaniel
 // @namespace    http://tampermonkey.net/
-// @version      2025-04-04
+// @version      2025-12-28
 // @supportURL   https://github.com/freezingDaniel/Kagi-assistant-full-width/issues
 // @match        https://kagi.com/assistant*
 // @icon         https://kagi.com/favicon-assistant-32x32.png
 // @grant        GM_addStyle
 // ==/UserScript==
 
-//GM_addStyle('.main-center-box > :first-child { max-width: calc(100% - 169px); !important }');
-GM_addStyle('.main-center-box > :first-child { max-width: 100%; !important }');
+GM_addStyle('.main-center-box > #chat_box { max-width: 100% !important }');
